56116e2364c935b668480f5b9769c7a9866e1001
eric9178 <eric9178vadim@gmail.com>
2021-02-24 04:20:48 +0900

    fixed

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', function () { return view('auth/login'); });
+Route::get('/login', function () { return view('auth/login'); })->name('login');
 Route::post('/login', [App\Http\Controllers\Auth\AuthController::class, 'login'])->name('login.submit');
 Route::get('/sign-in/{user}', [App\Http\Controllers\Auth\AuthController::class, 'signIn'])->name('sign-in');
 Route::get('/logout', [App\Http\Controllers\Auth\AuthController::class, 'logout'])->name('logout');
